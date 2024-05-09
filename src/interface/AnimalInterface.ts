eba5b89c30541cb6c867c1dcea48df2b73973d3a
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2024-05-09 15:38:20 -0300

    first commit

---
+++ src/interface/AnimalInterface.ts
@@ -0,0 +1,13 @@
+interface Animal {
+    id: number;
+    nome: string;
+    idade: string;
+    especie: string;
+    ra: string;
+    peso: number;
+    altura: number;
+    sexo: string;
+    dieta: string;
+    habitat: string;
+  }
+  
