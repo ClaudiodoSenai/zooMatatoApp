65e05a879b741ff858cbad23d3f6b59b2c5a5432
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2024-05-23 13:17:34 -0300

    LISTAGEM 80% ATUALIZADA 2024

--- App.tsx
+++ App.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Text, View } from "react-native";
 import CadastroAnimais from "./src/screens/CadastroAnimais";
 import ListagemAnimais from "./src/screens/ListagemAnimais";
-import EditAnimalPage from "./src/screens/EditarAnimais";
 
 function App(): JSX.Element {
 
    return (
-     <CadastroAnimais />
-   // <ListagemAnimais />
+    // <CadastroAnimais />
+    <ListagemAnimais />
    
    );
 
