547df6f880162d30978faa6368a431e5316d4a81
ClaudiodoSenai <claudiorobertojunior5@gmail.com>
2024-05-23 15:49:40 -0300

    nav

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Text, View } from "react-native";
 import CadastroAnimais from "./src/screens/CadastroAnimais";
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import ListagemAnimais from "./src/screens/ListagemAnimais";
 
 function App(): JSX.Element {
 
+  const Stack = createStackNavigator();
+
+  
    return (
-    //<CadastroAnimais />
-    <ListagemAnimais />
-   
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name={'Cadastro'} component={CadastroAnimais}
+        options={{ headerShown: false }} />
+
+      <Stack.Screen name={'Listagem'} component={ListagemAnimais}
+        options={{ headerShown: false }} />
+    </Stack.Navigator>
+  </NavigationContainer>
+
    );
 
 }
